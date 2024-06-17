ffd87b736d37fe121343ef32dbf539405cb08c35
Greg Doud <gpdoud@gmail.com>
2024-06-17 10:31:39 -0400

    Add project files.

---
+++ UsingTHis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace UsingTHis;
+
+internal class Program {
+
+    static void Main(string[] args) {
+
+        var s1 = new Student("Jane");
+        var s2 = new Student("Joe");
+
+        Console.WriteLine($"Score of 95 is an {Student.Grade(95)}");
+
+
+    }
+
+
+
+
+}
---
+++ UsingTHis/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingTHis;
+
+internal class Student {
+
+    private static int nextId { get; set; } = 1;
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public static string Grade(int score) {
+        if(score >= 92) {
+            return "A";
+        } else if(score >= 85 && score < 92) {
+            return "B";
+        }
+        return "?";
+    }
+
+    public Student(string Name) {
+        this.Id = nextId++;
+        this.Name = Name
+    }
+    
+}
